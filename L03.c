6140d6f7baa713694ba764dc98f6eda8e5448106
fexsen <michvan03@gmail.com>
2021-09-20 21:40:30 +0300

    Extended version of Lab_3

--- L03.c
+++ L03.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
-void Castling(int n){
-    const int L = 10;
-    int arr [L] = {10, 22, 11, 31, 2, 3, 7, 8, 9, 121};
-    for (int i = 0; i <= n-1; i++){
-        int c, b;
-        c = arr[i];
-        b = arr[L-i-1];
-        arr[i] = b;
-        arr[L-i-1] = c;
+void Castling(int n, int* p) {
+    while (n > 10) {
+        n--;
     }
-    for (int i = 0; i <= L-1; i++){
-        printf("%d\n", arr[i]);
+    for (int i = 0; i <= n - 1; i++) {
+        int c, b;
+        c = p[i];
+        b = p[10 - i - 1];
+        p[i] = b;
+        p[10 - i - 1] = c;
+    }
+    for (int i = 0; i <= 10 - 1; i++) {
+        printf("%d\n", p[i]);
     }
 }
 
-int main(){
-    Castling(3);
+int main(void) {
+    int arr[] = { 10, 22, 11, 31, 2, 3, 7, 8, 9, 121 };
+    int i;
+    printf("Vvedite chislo perestanovok:");
+    scanf("%d", &i);
+    Castling(i, arr);
 }
